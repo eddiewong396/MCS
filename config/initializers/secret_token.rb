35596bf2345bbd12f4f2ba1a473616f2b68cd3c5
Eddie Wong <isp396@gmail.com>
2013-02-12 10:33:10 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MCS::Application.config.secret_token = 'ecd8ed5117a4c18dc06ed39a295baf85e8243c19645ba212249e13b397ce52c27c141f7310a47c93defc7e73c90a14f27dafcb95b479b5af8a8b550c0e201ae9'
